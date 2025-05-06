bd80ab9f08883270c78f050eac06bae4b7e1ce55
Benja0g <benjamj3012@gmail.com>
2025-05-06 18:28:28 -0400

    actualización ultima clase

---
+++ modulos/funciones/cuadrilatero.py
@@ -0,0 +1,13 @@
+#Definir tres funciones.... calculo perimetro, area y volumen....
+
+def perimetro(ancho,largo):
+    return ancho + largo * 2
+    
+
+def area(ancho,largo):   
+    return ancho * largo
+    
+
+def volumen(ancho,largo,alto):
+    return ancho * largo * alto
+
--- modulos/principal.py
+++ modulos/principal.py
@@ -1,4 +1,34 @@
-import math
-pi = 3.1416
-print(pi)
-print(math.pi)
+import funciones.cuadrilatero
+
+def menu_principal():
+    print()
+    print("Cálculo de funciones geométricas.")
+    print("1: Perimetro")
+    print("2: Área")
+    print("3: Volumen")
+    print("0: Salir")
+    print()
+
+menu_principal()
+
+while True:
+    opcion = input("Seleccione su Opción (0-3): ")
+    if opcion == "1":
+      ancho = int(input("ingrese el ancho: "))
+      largo = int(input("ingrese el largo: "))
+      print(funciones.cuadrilatero.perimetro(ancho,largo))
+
+    elif opcion == "2":
+        ancho = int(input("ingrese el ancho: "))
+        largo = int(input("ingrese el largo: "))
+        print(funciones.cuadrilatero.area(ancho,largo))
+    elif opcion == "3":
+        ancho = int(input("ingrese el ancho: "))
+        largo = int(input("ingrese el largo: "))
+        alto = int(input("ingrese el alto: "))
+    elif opcion == "0":
+        print("saliendo del sistema...")
+        break
+    else:
+        print("opcion invalida")
+
