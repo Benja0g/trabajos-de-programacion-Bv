2c02b3759cfb87f5a969f8da8f1c33948ef3ab8d
Benja0g <benjamj3012@gmail.com>
2025-06-24 18:07:35 -0400

    atualizaciones clase 24-06-25

---
+++ gestion_notas/data/conexion.py
@@ -0,0 +1,15 @@
+import mysql.connector 
+
+conexion = mysql.connector.connect(
+    host = "127.0.0.1",
+    port = 3306,
+    user = "root",
+    database = "gestion_notas"
+)
+
+cursor = conexion.cursor()
+
+
+cursor.execute("SELECT * FROM docentes")
+resultado = cursor.fetchall()
+print(resultado)
